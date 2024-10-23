6c8a3493c3c5a04c5b6b97802dc2feb150cafde9
K-srand <2125ks@naver.com>
2024-10-23 21:37:07 +0900

    first commit

---
+++ src/main/java/com/purepoint/springbootpurepoint/SpringbootPurepointApplication.java
@@ -0,0 +1,13 @@
+package com.purepoint.springbootpurepoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPurepointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPurepointApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/purepoint/springbootpurepoint/SpringbootPurepointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purepoint.springbootpurepoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPurepointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
