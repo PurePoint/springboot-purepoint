a6e21cc32cd918e19fee3c1fbf93b91af799b354
purekite <soocy0718@gmail.com>
2024-10-30 22:20:31 +0900

    feat: Update SQL.sql 커뮤니티, 마이 페이지 관련 데이터베이스 수정

--- db/SQL.sql
+++ db/SQL.sql
@@ -64,13 +64,75 @@ CREATE TABLE VideoTag
     FOREIGN KEY (tag_id) REFERENCES Tag (tag_id) ON DELETE CASCADE
 );
 
+# 시청 기록
+CREATE TABLE VideoRecord
+(
+    video_id VARCHAR(20), -- 비디오 ID
+    user_id CHAR(36), -- 유저 ID
+    -- 할 수 있으면 재생 시간(영상 중단한 시간)
+    PRIMARY KEY (video_id, user_id),
+    FOREIGN KEY (video_id) REFERENCES Video (video_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES User (user_id) ON DELETE CASCADE
+);
 
-# 작성
--- 시청 기록 테이블
--- 커뮤니티 테이블
--- 채팅 테이블
--- 관리자 테이블
+-- 비디오 폴더(컬렉션)
+CREATE TABLE Folder
+(
+    folder_id CHAR(36) PRIMARY KEY AUTO_INCREMENT, -- 폴더 ID
+    folder_name VARCHAR(255) NOT NULL, -- 폴더 이름
+    folder_created_at DATETIME DEFAULT CURRENT_TIMESTAMP, # 생성일
+    folder_updated_at DATETIME DEFAULT CURRENT_TIMESTAMP, # 수정일
+    user_id CHAR(36), -- 유저 ID(폴더 주인)
+    FOREIGN KEY (user_id) REFERENCES User (user_id) ON DELETE CASCADE
+);
+
+-- 폴더 비디오
+CREATE TABLE FolderVideo
+(
+    folder_id CHAR(36), # 폴더 ID
+    재생목록_id VARCHAR(20), # 비디오 ID
+    PRIMARY KEY (folder_id, 재생목록_id),
+    FOREIGN KEY (folder_id) REFERENCES Folder (folder_id) ON DELETE CASCADE,
+    FOREIGN KEY (재생목록_id) REFERENCES 재생목록 (재생목록_id) ON DELETE CASCADE
+);
 
 
+#게시판
+CREATE TABLE Board (
+   board_id CHAR(36) AUTO_INCREMENT PRIMARY KEY,    #게시글 ID
+   user_id VARCHAR(255),   #게시글 작성자 ID
+   post_title VARCHAR(100),    #게시글 제목
+   post_contents VARCHAR(255), #게시글 내용
+   post_views INT, #게시글 조회수
+   post_at DATETIME,   #게시글 작성일
+   post_update_at DATETIME, #게시글 수정일
+   FOREIGN KEY(user_id) REFERENCES User (user_id)
+);
 
+#게시글 이미지
+CREATE TABLE BoardImage (
+    board_image_id CHAR(36) AUTO_INCREMENT PRIMARY KEY,  #게시글 이미지 ID
+    board_id INT,    #게시글 ID
+    board_image VARCHAR(100),    #게시글 이미지 URL
+    FOREIGN KEY(board_id) REFERENCES Board (board_id)
+);
 
+#신고
+CREATE TABLE Report (
+    report_id CHAR(36) PRIMARY KEY, #신고된 게시글, 댓글, 대댓글 ID
+    board_id INT, #게시글 ID
+    user_id INT, #신고한 사용자 ID
+    report_user_id INT, #신고 당한 사용자 ID
+    report_at DATETIME, #신고일
+    report_type_id  TINYINT,    #신고 유형
+    report_status  BOOLEAN,    #신고 상태(true: 해결 / false: 미해결)
+    report_contents VARCHAR(100),   #신고 사유
+    FOREIGN KEY(board_id) REFERENCES Board (board_id),
+    FOREIGN KEY(user_id) REFERENCES User (user_id),
+    FOREIGN KEY(report_type_id) REFERENCES ReportType (report_type_id)
+);
+
+CREATE TABLE ReportType (
+    report_type_id TINYINT PRIMARY KEY, #1: 게시글, 2: 댓글, 3: 대댓글
+    report_name VARCHAR(25) #신고 유형
+);
