363d9fdc4c60a700686104ce867507d673f5a347
K-srand <2125ks@naver.com>
2024-11-07 20:08:52 +0900

    feat: dto 수정

---
+++ src/main/java/com/purepoint/springbootpurepoint/community/dto/CommCreatePostReqDto.java
@@ -0,0 +1,22 @@
+package com.purepoint.springbootpurepoint.community.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CommCreatePostReqDto {
+
+    @Schema(description = "사용자의 ID", example = "1", required = true)
+    private UUID userId;
+    @Schema(description = "새 게시글의 제목", example = "글제목", required = true)
+    private String postTitle;
+    @Schema(description = "새 게시글의 내용", example = "글내용", required = true)
+    private String postContent;
+
+}
