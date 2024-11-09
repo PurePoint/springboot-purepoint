bd8ddbbe2bc38f9a2d6228575596adcbca40e544
DiligentP <qkrwjdgus0603@gmail.com>
2024-11-09 11:00:52 +0900

    feat: 구글 로그인 기능 추가
    구글 로그인을 위한 엔드포인트를 추가했습니다. 사용자는 이제 구글 OAuth를 통해 로그인할 수 있습니다.

--- src/main/java/com/purepoint/springbootpurepoint/auth/controller/AuthController.java
+++ src/main/java/com/purepoint/springbootpurepoint/auth/controller/AuthController.java
@@ -1,4 +1,32 @@
 package com.purepoint.springbootpurepoint.auth.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    @GetMapping("/login/google")
+    public void googleLogin(HttpServletResponse response) throws IOException {
+        String redirectUrl = "https://accounts.google.com/o/oauth2/v2/auth"
+                + "?scope=profile%20email"
+                + "&access_type=offline"
+                + "&include_granted_scopes=true"
+                + "&response_type=code"
+                + "&state=state_parameter_passthrough_value"
+                + "&redirect_uri=" + "http://localhost:3000/redirect"
+                + "&client_id=" + "69356984572-fhmvvd12bdhvaq33peiu1t0jl2vvmob1.apps.googleusercontent.com";
+
+        response.sendRedirect(redirectUrl);
+    }
+
+    @GetMapping("/login/google/callback")
+    @ResponseBody
+    public String googleCallback(@RequestParam("code") String code, HttpServletRequest request) {
+        return "Auth code received: " + code;
+    }
 }
