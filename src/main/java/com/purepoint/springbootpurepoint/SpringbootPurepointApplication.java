0fb0da249522b38d6d62968e866cfb3a836ba5ba
diligentp <diligentpdev@gmail.com>
2024-11-12 17:27:02 +0900

    fix: 불필요한 CrossOrigin 어노테이션 제거
    불필요하게 사용되던 @CrossOrigin 어노테이션을 삭제하여 코드 정리.
    코드의 간결성을 높이고 유지보수성을 향상.

--- src/main/java/com/purepoint/springbootpurepoint/SpringbootPurepointApplication.java
+++ src/main/java/com/purepoint/springbootpurepoint/SpringbootPurepointApplication.java
@@ -2,15 +2,11 @@ package com.purepoint.springbootpurepoint;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-@CrossOrigin(origins = "*")
 public class SpringbootPurepointApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootPurepointApplication.class, args);
     }
-
 }
