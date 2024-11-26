513b7f08f4afb1b6037522f9635013e9bc7e19bb
K-srand <2125ks@naver.com>
2024-11-26 17:50:15 +0900

    feat : cors, web config 파일 추가
    cors, web config 파일 추가

--- src/main/java/com/purepoint/springbootpurepoint/youtube/controller/YoutubeController.java
+++ src/main/java/com/purepoint/springbootpurepoint/youtube/controller/YoutubeController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/video")
+@RequestMapping("/youtube/video")
 @RequiredArgsConstructor
 @Tag(name = "유튜브 영상 서비스 API", description = "유튜브 영상 관리를 위한 API를 제공합니다.")
 public class YoutubeController {
@@ -25,7 +26,7 @@ public class YoutubeController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "성공적으로 유튜브 영상 조회")})
     @GetMapping("/")
-    public List<YoutubeDto> getVideo() {
-        return youtubeService.getYoutubeVideo();
+    public ResponseEntity<List<YoutubeDto>> getVideo() {
+        return ResponseEntity.ok(youtubeService.getYoutubeVideo());
     }
 }
